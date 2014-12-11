5035fb1bcb5c58a540a42fbf3c4bd747b980d3a7
Nick Pankow <nickpankow@gmail.com>
2014-12-10 19:42:25 -0800

    Added mouseover tooltip to graph
    Mouseover tool tip will display date, close, and moving average values
    at the point where the mouse is hovered over on the graph.

--- graph.js
+++ graph.js
@@ -28,6 +28,8 @@ var chart = d3.select(".chart")
 var dateMin = new Date("2013-01-01")
    ,dateMax = new Date("2013-12-31");
 
+var csvData = null;
+
 d3.csv("table_2013_avg.csv", type, function(error, data) {
 	// x.domain(data.map(function(d) { return d.date; }));
 	// x.domain([data[0].date,data[data.length - 1].date])
@@ -65,7 +67,9 @@ d3.csv("table_2013_avg.csv", type, function(error, data) {
 		.data(data)
 	  .enter().append('svg:path')
 	  .attr('d', lineAvgFunc(data))
-	  .attr("class", "line movingAvg")
+	  .attr("class", "line movingAvg");
+
+	csvData = data;
 });
 
 var legendRectSize = 18;
@@ -122,3 +126,101 @@ function type(d){
 	d.movingAvg = +d.movingAvg; // coerce to number
 	return d;
 }
+
+
+// Mouse over stuff
+var focus = chart.append("g")
+    .style("display", "none");
+
+focus.append("circle")
+	.attr("class", "y")
+	.style("fill", "none")
+	.style("stroke", "blue")
+	.attr("r", 4);
+
+chart.append("rect")
+	.attr("width", width)
+	.attr("height", height)
+	.style("fill", "none")
+	.style("pointer-events", "all")
+	.on("mouseover", function() { focus.style("display", null); })
+	.on("mouseout", function() { focus.style("display", "none"); })
+	.on("mousemove", mousemove);
+
+var bisectDate = d3.bisector(function(d) { return d.date; }).left;
+
+function mousemove() {
+	var x0 = x.invert(d3.mouse(this)[0]),
+		i = bisectDate(csvData, x0, 1),
+		d0 = csvData[i - 1],
+		d1 = csvData[i],
+		d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+
+	focus.select("circle.y")
+		.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
+
+	// focus.select("text.y1")
+	// 	.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")")
+	// 	.text("Close: " + d.close);
+	focus.select("text.y2")
+		.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")")
+		.text("Close: " + d.close);
+	// focus.select("text.y3")
+	// 	.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")")
+	// 	.text(formatDate(d.date));
+	focus.select("text.y4")
+		.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")")
+		.text("Date: " + formatDate(d.date));
+
+	focus.select("text.y5")
+		.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")")
+		.text("Moving Avg: " + d.movingAvg);
+
+	focus.select(".x")
+		.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")")
+		.attr("y2", height - y(d.close));
+	focus.select(".y")
+		.attr("transform", "translate(" + width * -1 + "," + y(d.close) + ")")
+		.attr("x2", width + width);
+}
+
+// Mouse over tooltip
+var formatDate = d3.time.format("%d-%b");
+focus.append("line")
+	.attr("class", "y")
+	.style("stroke", "blue")
+	.style("stoke-dasharray", "3,3")
+	.style("opacity", 0.5)
+	.attr("x1", width)
+	.attr("x2", width);
+
+// focus.append("text")
+// 	.attr("class", "y1")
+// 	.style("stroke", "white")
+// 	.style("stroke-width", "3.5px")
+// 	.style("opacity", 0.8)
+// 	.attr("dx", 8)
+// 	.attr("dy", "-.3em");
+
+focus.append("text")
+	.attr("class", "y2")
+	.attr("dx", 8)
+	.attr("dy", "-2em");
+
+// focus.append("text")
+// 	.attr("class", "y3")
+// 	.attr("stroke", "white")
+// 	.style("stroke-width", "3.5px")
+// 	.style("opacity", 0.8)
+// 	.attr("dx", 8)
+// 	.attr("dy", "1em");
+
+focus.append("text")
+	.attr("class", "y4")
+	.attr("dx", 8)
+	.attr("dy", "-3em");
+
+focus.append("text")
+	.attr("class", "y5")
+	.attr("dx", 8)
+	.attr("dy", "-1em");
